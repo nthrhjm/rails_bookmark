6dd8ddd296d8162867638c4c18233ebb3b719724
Hajime Nutahara <hajime.nutahara@gmail.com>
2019-12-21 13:31:17 +0900

    入力されたurlからtitleを取得するfetch_titleメソッドをBookmarkモデルに追加

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,2 +1,15 @@
 class Bookmark < ApplicationRecord
+  before_save :fetch_title
+
+  def fetch_title
+    begin
+      url = URI.parse(url)
+      response = Net::HTTP.get(uri).force_encoding(Encoding::UTF_8)
+      capture = /<title>(?<title>.+)<\/title>/m.match(response).named_captures
+      self.title = captures['title']
+    rescue => e
+      logger.warn "Fetch Title Failed.url = [#{url}],error = #{e.message}"
+    end
+    false
+  end
 end
