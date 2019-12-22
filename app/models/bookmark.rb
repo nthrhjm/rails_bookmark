0ba183728db98809150f5851c6b52a7ea5dbc9da
Hajime Nutahara <hajime.nutahara@gmail.com>
2019-12-22 17:19:24 +0900

    fix boolmark.rb

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -5,7 +5,7 @@ class Bookmark < ApplicationRecord
     begin
       uri = URI.parse(url)
       response = Net::HTTP.get(uri).force_encoding(Encoding::UTF_8)
-      captures   = /<title>(?<title>.+)<\/title>/m.match(response).named_captures
+      captures = /<title>(?<title>.+)<\/title>/m.match(response).named_captures
       self.title = captures['title']
     rescue => e
       logger.warn "Fetch Title Failed.url = [#{url}],error = #{e.message}"
